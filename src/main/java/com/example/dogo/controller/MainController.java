2ff64edf9b29193045c4e6e2a15bced3edf1597e
rotus <rbfla0426_1@naver.com>
2026-05-10 21:31:13 +0900

    index, base 분리
    헤더 및 사이드바 분리
    css, js 분리

--- src/main/java/com/example/dogo/controller/MainController.java
+++ src/main/java/com/example/dogo/controller/MainController.java
@@ -1,10 +1,12 @@
 package com.example.dogo.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class MainController {
   @GetMapping("/")
-  public String index() {
+  public String index(Model model) {
+    model.addAttribute("currentUri", "/"); // 메인 페이지 표시
     return "index";
   }
 }
---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,4 @@
+function hideLogo() {
+    document.getElementById('logoImg').style.display = 'none';
+    document.getElementById('logoText').style.display = 'inline-block';
+}
