0f4cbb59bd83989ef441369ec2fb141a998ef336
kominjung04 <heppy8888@naver.com>
2026-05-11 14:26:45 +0900

    commit

--- src/main/java/com/example/dogo/controller/MainController.java
+++ src/main/java/com/example/dogo/controller/MainController.java
@@ -1,12 +1,15 @@
 package com.example.dogo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MainController {
+
   @GetMapping("/")
   public String index(Model model) {
-    model.addAttribute("currentUri", "/"); // 메인 페이지 표시
+    model.addAttribute("currentUri", "/");
     return "index";
   }
 }
