87dae2d768fcda1513a59d0a73ff5165592f07a6
rotus <rbfla0426_1@naver.com>
2026-05-11 23:26:58 +0900

    index 검색 form 추가, 로그인시 마이페이지버튼 추가

--- src/main/java/com/example/dogo/controller/MainController.java
+++ src/main/java/com/example/dogo/controller/MainController.java
@@ -1,12 +1,21 @@
 package com.example.dogo.controller;
-
+ 
+import com.example.dogo.service.LostItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+ 
+@Controller
+@RequiredArgsConstructor
 public class MainController {
+ 
+  private final LostItemService lostItemService;
+ 
   @GetMapping("/")
   public String index(Model model) {
     model.addAttribute("currentUri", "/"); // 메인 페이지 표시
+    model.addAttribute("searchCategories", lostItemService.getSearchCategoryNames());
     return "index";
   }
 }
