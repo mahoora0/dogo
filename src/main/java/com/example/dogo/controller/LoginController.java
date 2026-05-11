e7f3f58a1a2cfea0bcd8f1877ad757e95553e34a
SimSeungAh <ng7463@naver.com>
2026-05-11 16:57:11 +0900

    로그인페이지UI

---
+++ src/main/java/com/example/dogo/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.example.dogo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+  @GetMapping("/login")
+  public String loginPage() {
+    return "login"; // templates/login.html을 찾아감
+  }
+}
