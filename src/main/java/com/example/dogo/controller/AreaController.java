0f43ea235703fd19744830ea61f7d85fc26e081d
jeongwon <126747344+mahoora0@users.noreply.github.com>
2026-05-11 11:32:18 +0900

    지도 불러오기

--- src/main/java/com/example/dogo/controller/AreaController.java
+++ src/main/java/com/example/dogo/controller/AreaController.java
@@ -1,16 +1,21 @@
 package com.example.dogo.controller;
 
-import org.springframework.stereotype.Controller; // RestController가 아님!
+import com.example.dogo.service.AreaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller // HTML 템플릿을 찾기 위해 필수
+@Controller
+@RequiredArgsConstructor
 public class AreaController {
 
-  @GetMapping("/areas/list") // 브라우저 접속 주소
+  private final AreaService areaService;
+
+  @GetMapping("/areas/list")
   public String areaList(Model model) {
     model.addAttribute("currentUri", "/areas/list");
-    // 필요한 경우 서비스에서 데이터를 가져와 모델에 추가
-    return "area/list"; // src/main/resources/templates/area/list.html 실행
+    model.addAttribute("areas", areaService.getActiveAreas());
+    return "area/list";
   }
 }
